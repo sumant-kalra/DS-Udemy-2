ab2f0a6e7f3f350d2a7a8aeb9b577bfcf1e5c3b7
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-06-01 22:22:20 +0530

    chg: dev: TOH improved

--- src/76_TowerOfHanoi.cpp
+++ src/76_TowerOfHanoi.cpp
@@ -1,4 +1,4 @@
-#define TOWER_OF_HANOI_MAIN 1
+#define TOWER_OF_HANOI_MAIN 0
 #if TOWER_OF_HANOI_MAIN
 
 #include <iostream>
@@ -21,6 +21,32 @@ static void toh(int n, int A, int C, int B)
     toh(n - 1, A, B, C);
     toh(1, A, C, B); // Note that this can be removed and replaced by the code under condition n==1
     toh(n - 1, B, C, A);
+}
+
+static void toh2(int n, int A, int C, int B)
+{
+    if (n < 1)
+    {
+        std::cerr << "[ERROR] the number of discs must be greater than 0\n";
+        exit(-1);
+    }
+
+    if (n == 1)
+    {
+        std::cout << A << " -> " << C << "\n";
+        return;
+    }
+
+    if (n == 2)
+    {
+        std::cout << A << " -> " << B << "\n";
+        std::cout << A << " -> " << C << "\n";
+        std::cout << B << " -> " << C << "\n";
+        return;
+    }
+    toh2(n - 1, A, B, C);
+    std::cout << A << " -> " << C << "\n";
+    toh2(n - 1, B, C, A);
 }
 
 int main(int argc, char const *argv[])
