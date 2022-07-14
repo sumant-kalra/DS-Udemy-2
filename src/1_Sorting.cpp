20d10acccd98c32fdd678a5bf41d23ea8af40c03
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-07-15 02:22:30 +0530

    chg: dev: Insertion sort implemented

--- src/1_Sorting.cpp
+++ src/1_Sorting.cpp
@@ -2,11 +2,13 @@
 
 void sort_swap(int &a, int &b);
 void bubbleSort(int *arr, size_t size);
+void insertionSort(int *arr, size_t size);
 
 int main(int argc, char const *argv[])
 {
     int arr[6] = {5, 9, 8, 3, 4, 7};
-    bubbleSort(arr, 6);
+    // bubbleSort(arr, 6);
+    insertionSort(arr, 6);
 
     for (size_t i = 0; i < 6; ++i)
         std::cout << arr[i] << " ";
@@ -38,5 +40,17 @@ void bubbleSort(int *arr, size_t size)
         }
         if (isSorted)
             break;
+    }
+}
+
+void insertionSort(int *arr, size_t size)
+{
+    for (int i = 1; i < size; ++i)
+    {
+        for (int j = i; j > 0; --j)
+        {
+            if (arr[j] < arr[j - 1])
+                sort_swap(arr[j], arr[j - 1]);
+        }
     }
 }
